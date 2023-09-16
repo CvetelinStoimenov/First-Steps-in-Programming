292198a8ec7663dc148923f1a9777fa83ccd36c7
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-09-16 14:54:27 +0300

    New program

---
+++ 03.DepositCalculator/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _03.DepositCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double depositedAmount = double.Parse(Console.ReadLine());
+            int termOfDeposi = int.Parse(Console.ReadLine());
+            double annualInterestRate = double.Parse(Console.ReadLine());
+            double rate = annualInterestRate / 100;
+
+
+            //amount = deposited amount + term of deposit *(deposited amount* annual interest rate) / 12
+
+            double amount = depositedAmount + termOfDeposi * (depositedAmount * rate) / 12;
+            Console.WriteLine(amount);
+        }
+    }
+}
