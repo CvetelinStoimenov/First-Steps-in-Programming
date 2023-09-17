e21e77128ee6f7554042a45519ab608b429734b7
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-09-17 12:39:59 +0300

    new

---
+++ 08. BasketballEquipment/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Diagnostics;
+
+namespace _08._BasketballEquipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             8. Basketball equipment
+
+                Jesse decides he wants to play basketball, but he needs equipment to train. Write a program that calculates the expenses of Jesse if he starts training, 
+                knowing how much is the fee for basketball training for a period of 1 year.
+                • Basketball sneakers – their price is 40% less than the fee for one year
+                • Basketball team – its price is 20% cheaper than that of sneakers
+                • Basketball – its price is 1 / 4 of the price of the basketball team
+                • Basketball accessories – their price is 1 / 5 of the price of the basketball
+
+                Input
+                From the console read 1 row:
+                · The annual basketball training fee – an integer in the range [0... 9999]
+                Output
+                Print on the console how much Jesse will spend if he starts playing basketball.
+
+                Example Input/Output 
+              Input 365
+
+             Output 811.76
+                    Price of training per year: 365
+                    Price of basketball sneakers: 365 – 40% = 219
+                    Price per basketball team: 219 – 20% = 175.20
+                    Price of basketball: 1 / 4 from 175.20 = 43.80
+                    Price of basketball accessories: 1 / 5 from 43.80 = 8.76
+                    Total price for the equipment: 365 + 219 + 175.20 + 43.80 + 8.76 = 811.76
+
+             Input 550
+
+             Output 1223.2
+                    Price of training per year: 550
+                    Price of basketball sneakers: 550 – 40% = 330
+                    Price per basketball team: 330 – 20% = 264
+                    Basketball price: 1/4 of 264 = 66
+                    Price of basketball accessories: 1 / 5 from 66 = 13.20
+                    Total price for the equipment: 550 + 330 + 264 + 66 + 13.20 = 1223.2
+             
+             */
+
+            int trainingFee = int.Parse(Console.ReadLine());
+
+            //Price of training per year: 365
+            //Price of basketball sneakers: 365 – 40 % = 219
+           
+            //double sneakers = trainingFee * 0.6;
+            double sneakers = trainingFee - (trainingFee * 0.4);
+
+            //Price per basketball team: 219 – 20 % = 175.20
+
+            //double team = sneakers * 0.8;
+            double team = sneakers - (sneakers * 0.2);
+
+            //Price of basketball: 1 / 4 from 175.20 = 43.80
+
+            double basketball = team * 0.25;
+
+            //Price of basketball accessories: 1 / 5 from 43.80 = 8.76
+
+            double accessories = basketball * 0.2;
+
+            //Total price for the equipment: 365 + 219 + 175.20 + 43.80 + 8.76 = 811.76
+
+            double TotalCost = trainingFee + sneakers + team + basketball + accessories;
+
+            Console.WriteLine(TotalCost);
+
+        }
+    }
+}
---
+++ 09.Aquarium/Program.cs
@@ -0,0 +1,78 @@
+﻿namespace _09.Aquarium
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             9. Aquarium
+
+                For his birthday, Lyubomir received an aquarium in the shape of a parallelepiped. Initially, 
+                we read from the console in separate rows its dimensions – length, width and height in centimeters. 
+                It is necessary to calculate how many liters of water the aquarium will collect, 
+                if it is known that a certain percentage of its capacity is occupied by sand, plants, heater and pump.
+
+                One liter of water is equal to one cubic decimeter (1 l = 1 dm3).
+                Write a program that calculates the liters of water that are needed to fill the aquarium.
+                Input
+                From the console read 4 lines:
+                1. Length in cm – an integer number in the range [10 ... 500]
+                2. Width in cm – an integer number in the range [10 ... 300]
+                3. Height in cm – an integer number in the range [10... 200]
+                4. Percentage – floating point number in the range [0.000 ... 100.000]
+
+                Output
+                Print one number on the console:
+                · the liters of water that the aquarium will collect, rounded to the second decimal place.
+                Example Input/Output 
+                         
+            Input 85
+                  75
+                  47
+                  17
+
+             Output 248.69
+                    Volume of the aquarium: 85 * 75 * 47 = 299625 cm3
+                    Volume in liters: 299625 * 0.001 or 299625 / 1000 => 299.625 liters
+                    Occupied space: 17% = 0.17
+                    Required liters: 299.625 * (1 - 0.17) = 248.68875 liters
+
+            Input 105
+                  77
+                  89
+                  18.5
+
+             Output 586.45
+                    Volume of the aquarium: 105 * 77 * 89 = 719565 cm3
+                    Volume in liters: 719565 * 0.001 or 719565 / 1000 => 719.565 liters
+                    Occupied space: 18.5% = 0.185
+                    Required liters: 719.565 * (1 - 0.185) = 586.445475 liters
+             
+             */
+
+
+            int Length = int.Parse(Console.ReadLine());
+            int Width = int.Parse(Console.ReadLine());
+            int Height = int.Parse(Console.ReadLine());
+            float Percentage = float.Parse(Console.ReadLine());
+
+            //Volume of the aquarium: 85 * 75 * 47 = 299625 cm3
+
+            double aquariumVol = Length * Width * Height;
+
+            //Volume in liters: 299625 * 0.001 or 299625 / 1000 => 299.625 liters
+
+            double VolLiters = aquariumVol / 1000;
+
+            //Occupied space: 17 % = 0.17
+
+            double OccupiedSpace = Percentage / 100;
+
+            //Required liters: 299.625 * (1 - 0.17) = 248.68875 liters
+
+            double Requiredliters = VolLiters * (1 - OccupiedSpace);
+
+            Console.WriteLine("{0:0.00}", Requiredliters);
+        }
+    }
+}
