4f154a79eaf51fa2fee48c28efe2c67143f45760
Mishenata <45821593+Mishenata@users.noreply.github.com>
2023-09-17 11:58:48 +0300

    add new

--- 06.Redecorating/Program.cs
+++ 06.Redecorating/Program.cs
@@ -5,12 +5,14 @@
         static void Main(string[] args)
         {
             /*6. Redecorating
-            Rumen wants to repaint the living room and has hired craftsmen for this purpose. Write a program that calculates the cost of the repair, taking the following prices for the calculation:
+            Rumen wants to repaint the living room and has hired craftsmen for this purpose. Write a program that calculates the cost of the repair, 
+            taking the following prices for the calculation:
             · Protective nylon - 1.50 BGN per square meter
             · Paint - 14.50 BGN per liter
             · Paint thinner - 5.00 BGN per liter
 
-            Just in case, to the necessary materials, Rumen wants to add another 10% of the amount of paint and 2 square meters of nylon, also 0.40 leva for bags. The amount paid to the craftsmen for 1 hour of work is equal to 30% of the sum of all material costs.
+            Just in case, to the necessary materials, Rumen wants to add another 10% of the amount of paint and 2 square meters of nylon, also 0.40 leva for bags.
+            The amount paid to the craftsmen for 1 hour of work is equal to 30% of the sum of all material costs.
 
             Input
             The input is read from the console and contains exactly 4 lines:
@@ -50,6 +52,38 @@
                     Amount for masters: (220.40 * 30%) * 1 = 66.12 BGN 
                     Total: 220.40 + 66.12 = 286.52 BGN
              */
+
+            int nylon = int.Parse(Console.ReadLine());
+            int paint = int.Parse(Console.ReadLine());
+            int thinner = int.Parse(Console.ReadLine());
+            int Hours = int.Parse(Console.ReadLine());
+
+            //Nylon amount: (10 + 2) * 1.50 = 18 BGN
+
+            double nylonPrice = (nylon + 2) * 1.5;
+
+            //Amount for paint: (11 + 10 %) * 14.50 = 175.45 BGN
+
+            double paintPrice = (paint * 1.1) * 14.50;
+
+            //Amount for thinner: 4 * 5.00 = 20.00 BGN Amount for bags: 0.40 BGN
+
+            double thinnerPrice = thinner * 5;
+
+            //Total amount for materials: 18 + 175.45 + 20.00 + 0.40 = 213.85 BGN
+
+            double totalAmountMaterials = nylonPrice + paintPrice + thinnerPrice + 0.40;
+
+            //Amount for craftsmen: (213.85 * 30 %) * 8 = 513.24 BGN
+
+            double craftsmen = totalAmountMaterials * 0.3;
+
+            //Total amount: 213.85 + 513.24 = 727.09 BGN
+
+            double totalAmount = totalAmountMaterials + craftsmen * Hours;
+
+            Console.WriteLine(totalAmount);
+
         }
     }
 }
---
+++ 07.FoodDelivery/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace _07.FoodDelivery
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             The restaurant opens its doors and offers several menus at preferential prices:
+            • Chicken menu – 10.35 lv.
+            • Menu with fish – 12.40 lv.
+            • Vegetarian menu – 8.15 lv.
+
+            Write a program that calculates how much it will cost a group of people to order takeaways.
+            The group will also order a dessert, the price of which is equal to 20% of the total bill (excluding delivery).
+            The delivery price is 2.50 BGN and is finally charged.
+
+            Input
+            From the console read 3 lines:
+            · Number of chicken menus – integer in the range [0 ... 99]
+            · Number of menus with fish – integer in the range [0 ... 99]
+            · Number of vegetarian menus – an integer in the range [0 ... 99]
+
+            Output
+            Print out only one line on the console: "{order price}"
+            Example Input/Output
+
+              Input 2
+                    4
+                    3
+
+             Output 116.2
+                    Price for chicken menus: 2 pieces * 10.35 = 20.70
+                    Price for the menus with fish: 4 pieces * 12.40 = 49.60
+                    Price for vegetarian menus: 3 pieces * 8.15 = 24.45
+                    Total cost of menus: 20.70 + 49.60 + 24.45 = 94.75 
+                    Price of dessert: 20% from 94.75 = 18.95
+                    Delivery price: 2.50 (conditional)
+                    Total order price: 94.75 + 18.95 + 2.50 = 116.20
+
+              Input 9
+                    2
+                    6
+
+             Output 202.72
+                    Price for chicken menus: 9 pieces * 10.35 = 93.15
+                    Price for fish menus: 2 pieces * 12.40 = 24.80
+                    Price for vegetarian menus: 6 pieces * 8.15 = 48.90
+                    Total cost of menus: 93.15 + 24.80 + 48.90 = 166.85 
+                    Price of dessert: 20% from 166.85 = 33.37
+                    Delivery price: 2.50 (conditional)
+                    Total order price: 166.85 + 33.37 + 2.50 = 202.72
+
+             */
+
+            int chickenMenus = int.Parse(Console.ReadLine());
+            int fishMenus = int.Parse(Console.ReadLine());
+            int vegeMenus = int.Parse(Console.ReadLine());
+
+            //Price for chicken menus: 9 pieces * 10.35 = 93.15
+
+            double chickenMenusPrice = chickenMenus * 10.35;
+
+            //Price for fish menus: 2 pieces * 12.40 = 24.80
+
+            double fishMenusPrice = fishMenus * 12.40;
+
+            //Price for vegetarian menus: 6 pieces * 8.15 = 48.90
+
+            double vegeMenusPrice = vegeMenus * 8.15;
+
+            //Total cost of menus: 93.15 + 24.80 + 48.90 = 166.85
+
+            double TotalMenuCost = chickenMenusPrice + fishMenusPrice + vegeMenusPrice;
+
+            //Price of dessert: 20 % from 166.85 = 33.37
+
+            double dessertPrice = TotalMenuCost * 0.2;
+
+            //Delivery price: 2.50(conditional)
+
+            //Total order price: 166.85 + 33.37 + 2.50 = 202.72
+
+            double TotalCost = TotalMenuCost + dessertPrice + 2.5;
+
+
+Console.WriteLine(TotalCost);
+        }
+    }
+}
